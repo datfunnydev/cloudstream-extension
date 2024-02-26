3fb7ad2eec5fb9a5050b580063aee62f19fbe1ac
John Smith <dn16092000@gmail.com>
2024-02-26 16:39:41 +0700

    Update

--- build.gradle.kts
+++ build.gradle.kts
@@ -38,7 +38,7 @@ subprojects {
         // when running through github workflow, GITHUB_REPOSITORY should contain current repository name
         setRepo(System.getenv("GITHUB_REPOSITORY") ?: "https://github.com/datfunnydev/cloudstream-extension-core")
 
-        authors = listOf("Hexated")
+        authors = listOf("datfunnydev")
     }
 
     android {
